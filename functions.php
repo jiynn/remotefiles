f2900935a22ac198c3d88efebf6f2b242912dff8
jiynn <33206565+jiynn@users.noreply.github.com>
2024-09-01 00:46:11 -0400

    Update functions.php
    added deployment authorization

--- functions.php
+++ functions.php
@@ -128,6 +128,25 @@ function get_lead_assignment_stats($conn, $users) {
     return $stats;
 }
 
+function verify_authorized_deployment() {
+    $local_key_file = __DIR__ . '/deployment_key.txt';
+    $remote_keys_url = 'https://raw.githubusercontent.com/jiynn/remotefiles/main/authorized_keys.php';
+
+    $local_key = trim(file_get_contents($local_key_file));
+    $remote_keys = file_get_contents($remote_keys_url);
+    $authorized_keys = json_decode($remote_keys, true);
+
+    $hashed_local_key = hash('sha256', $local_key);
+
+    foreach ($authorized_keys as $org => $hashed_key) {
+        if ($hashed_key === $hashed_local_key) {
+            return true;
+        }
+    }
+
+    die('Unauthorized deployment detected. Access denied.');
+}
+
 function assign_leads($conn) {
     $users = get_all_users($conn);
     $assigned_count = 0;
