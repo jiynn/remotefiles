590864517b3f46582cc123166f50a8e1ba8f9cf4
jiynn <33206565+jiynn@users.noreply.github.com>
2024-09-01 12:14:02 -0400

    Update remote_keys.php

--- remote_keys.php
+++ remote_keys.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 
 $hashedKeys = [
     '$2y$10$lyh7tCgDKaDLQJuY9ZXnSeMbxWRO7kflQ6NTmYnsWaSgkWgrvs0OS',
-    '$2y$10$abcdefghijk1234567890.ABCDEFGHIJKLMNOPQRSTUVWXYZ012345',
+    '$2y$10$7LK62jWplzObhgljJKTWv.pLTol48/iQYpzkfT9sTb9f3R08oUT.a',
     '$2y$10$uvwxyzABCDEFGHIJKLMNOP.QRSTUVWXYZ0123456789abcdefghijk'
 ];
 
