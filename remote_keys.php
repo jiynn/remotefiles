dfd418e84ac0f9e2c3300ed01306526103455cff
jiynn <33206565+jiynn@users.noreply.github.com>
2024-09-01 22:18:19 -0400

    Update remote_keys.php

--- remote_keys.php
+++ remote_keys.php
@@ -1,5 +1,8 @@
-[
+$hashedKeys = [
     '$2y$10$lyh7tCgDKaDLQJuY9ZXnSeMbxWRO7kflQ6NTmYnsWaSgkWgrvs0OS',
     '$2y$10$7LK62jWplzObhgljJKTWv.pLTol48/iQYpzkfT9sTb9f3R08oUT.a',
+    '$2y$10$QZHNMKLJIhGFEDCBA9876uXWVUTSRQPONMLKJIHGFEDCBA98765',
     '$2y$10$QZHNMKLJIhGFEDCBA9876uXWVUTSRQPONMLKJIHGFEDCBA98765'
-]
+];
+
+echo json_encode($hashedKeys);
